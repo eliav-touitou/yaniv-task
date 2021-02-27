8f5fa0c21c1bd2a26b8b2bdd6a6d7fba4bee486e
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-02-27 20:28:23 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const d = new Deck();
+d.createCardsDeck();
+let players = [
+  new Player("avi", 1, d, 0),
+  new Player("avia", 2, d, 0),
+  new Player("aviad", 3, d, 0),
+  new Player("aviel", 4, d, 0),
+];
+console.log(d);
+console.log(players[0]);
+
+window.addEventListener("DOMContentLoaded", () => {
+  printGame(players);
+});
